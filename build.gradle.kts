79dd65ce9defbfa6c38d80b2c6359610bfeaaaf7
Chlod Alejandro <chlod@chlod.net>
2025-10-27 03:07:17 +0800

    fix plugin.yml version not automatically updating

--- build.gradle.kts
+++ build.gradle.kts
@@ -85,6 +85,10 @@ tasks {
         minecraftVersion("1.21.4")
     }
 
+    build {
+        dependsOn("shadowJar")
+    }
+
     test {
         useJUnitPlatform()
     }
@@ -96,22 +100,17 @@ tasks {
             exclude(dependency("org.apache.logging.log4j:log4j-\\w+:.*"))
         }
     }
+
+    processResources {
+        val props = mapOf("version" to version)
+        inputs.properties(props)
+        filesMatching("plugin.yml") {
+            expand(props)
+        }
+    }
 }
 
 val targetJavaVersion = 21
 kotlin {
     jvmToolchain(targetJavaVersion)
-}
-
-tasks.build {
-    dependsOn("shadowJar")
-}
-
-tasks.processResources {
-    val props = mapOf("version" to version)
-    inputs.properties(props)
-    filteringCharset = "UTF-8"
-    filesMatching("plugin.yml") {
-        expand(props)
-    }
 }
