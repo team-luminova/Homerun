402bf6a60c690c01a82a4ad64b0ed20a3ef4dc9e
Chlod Alejandro <chlod@chlod.net>
2026-03-02 04:14:50 +0800

    add duration helper

--- src/main/kotlin/net/chlod/minecraft/homerun/config/warnings/ChatMessageWarningMethod.kt
+++ src/main/kotlin/net/chlod/minecraft/homerun/config/warnings/ChatMessageWarningMethod.kt
@@ -3,7 +3,7 @@ package net.chlod.minecraft.homerun.config.warnings
 import net.chlod.minecraft.homerun.Homerun
 import net.chlod.minecraft.homerun.config.ResetRule
 import net.chlod.minecraft.homerun.config.conditions.ResetCondition
-import net.kyori.adventure.text.Component
+import net.chlod.minecraft.homerun.helpers.DurationHandler
 import net.kyori.adventure.text.format.NamedTextColor
 import net.kyori.adventure.text.minimessage.tag.resolver.Formatter
 import net.kyori.adventure.text.minimessage.tag.resolver.Placeholder
@@ -145,54 +145,11 @@ class ChatMessageWarningMethod() : ResetWarningMethod(ResetWarningMethodType.CHA
 
         val nextReset = condition.getNextReset(plugin)!!
 
-        val hoursUntil = (referenceIntervalMillis / 3600000) % 24
-        val minutesUntil = (referenceIntervalMillis / 60000) % 60
-        val secondsUntil = (referenceIntervalMillis / 1000) % 60
-
-        val durations = mutableListOf<Component>()
-
-        if (hoursUntil > 0) {
-            durations.add(
-                plugin.messages.get(
-                    "warning-chat-hours",
-                    Placeholder.unparsed("num", hoursUntil.toString()),
-                    Formatter.choice("unit", hoursUntil)
-                )
-            )
-        }
-        if (minutesUntil > 0) {
-            durations.add(
-                plugin.messages.get(
-                    "warning-chat-minutes",
-                    Placeholder.unparsed("num", minutesUntil.toString()),
-                    Formatter.choice("unit", minutesUntil)
-                )
-            )
-        }
-        if (secondsUntil > 0) {
-            durations.add(
-                plugin.messages.get(
-                    "warning-chat-seconds",
-                    Placeholder.unparsed("num", secondsUntil.toString()),
-                    Formatter.choice("unit", secondsUntil)
-                )
-            )
-        }
-
-        if (durations.isEmpty()) {
-            // Weird?
-            return
-        }
-
         val text = plugin.messages.get(
             "warning-chat",
             Placeholder.component(
                 "duration",
-                durations.reduce { acc, component ->
-                    acc.append(
-                        plugin.messages.get("warning-chat-glue")
-                    ).append(component)
-                }
+                DurationHandler(plugin).asTextComponent(referenceIntervalMillis) ?: return
             ),
             Formatter.date(
                 "date",
